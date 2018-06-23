2c0e45d7320ba1220ff391712df376939c494c8a
lorentag <lorenzo90@gmail.com>
2018-06-23 14:15:08 -0700

    List and Sounds redy

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,8 @@ import { NavController } from 'ionic-angular';
 import { ANIMALES } from '../../data/data.animales';
 import { Animal } from '../../interfaces/animal.interface';
 
+import { Refresher, reorderArray } from 'ionic-angular';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,25 +12,79 @@ import { Animal } from '../../interfaces/animal.interface';
 export class HomePage {
 
   animals: Animal[] = [];
+  audio = new Audio();
+  audioTime: any;
+
+  ordering: boolean = false;
 
   constructor(public navCtrl: NavController) {
+    console.log('pinga');
 
-    this.animals = ANIMALES.splice(0);
+    this.animals = ANIMALES.slice(0);
+
 
   }
 
 
   reproducir(animal: Animal) {
-    console.log(animal);
-    let audio = new Audio();
-    audio.src = animal.audio;
 
-    audio.load();
-    audio.play();
+    this.pause_audio(animal);
+
+    if (animal.reproduciendo) {
+      animal.reproduciendo = false;
+      return;
+    }
+
+    this.audio.src = animal.audio;
+
+    this.audio.load();
+    this.audio.play();
 
     animal.reproduciendo = true;
 
-    setTimeout( ()=> animal.reproduciendo = false, animal.duracion * 1000);
+    this.audioTime = setTimeout(() => animal.reproduciendo = false, animal.duracion * 1000);
   }
+
+  private pause_audio(animalSel: Animal) {
+
+    clearTimeout(this.audioTime);
+    this.audio.pause();
+    this.audio.currentTime = 0;
+
+    for (let animal of this.animals) {
+      if (animal.nombre != animalSel.nombre) {
+        animal.reproduciendo = false;
+      }
+    }
+
+  }
+
+  deleteAnimal(idx) {
+    this.animals.splice(idx, 1);
+  }
+
+  doRefresh($event: Refresher) {
+    setTimeout(() => {
+      this.animals = ANIMALES.slice(0);
+      $event.complete();
+    }, 1500);
+  }
+
+  reorderAnimal(indexes: any) {
+
+    // the event is returned with wrong indexes
+
+    let idx = {
+      from: indexes.from - 2,
+      to: indexes.to - 2
+    };
+
+   this.animals = reorderArray(this.animals, idx);
+
+
+
+  }
+
+
 
 }
