30b22cf850a132a94633da7dc5edf65923e59ad2
lorentag <lorenzo90@gmail.com>
2018-06-23 12:23:34 -0700

    List and Sounds

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ANIMALES } from '../../data/data.animales';
+import { Animal } from '../../interfaces/animal.interface';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  animals: Animal[] = [];
+
   constructor(public navCtrl: NavController) {
 
+    this.animals = ANIMALES.splice(0);
+
+  }
+
+
+  reproducir(animal: Animal) {
+    console.log(animal);
+    let audio = new Audio();
+    audio.src = animal.audio;
+
+    audio.load();
+    audio.play();
+
+    animal.reproduciendo = true;
+
+    setTimeout( ()=> animal.reproduciendo = false, animal.duracion * 1000);
   }
 
 }
